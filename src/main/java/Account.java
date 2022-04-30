d5c3084a2724d1c145c2e342091cdeb4610aac91
Mark Glushkov <mark@glushkov.cc>
2022-04-30 15:29:28 +0400

    Tests were updated.

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,3 +1,5 @@
+import io.qameta.allure.Step;
+
 public class Account {
 
     private final String name;
@@ -6,25 +8,12 @@ public class Account {
         this.name = name;
     }
 
+    @Step("Проверяем, подходит ли имя для размещения на карте")
     public boolean checkNameToEmboss() {
-        /*
-             Этот метод должен проверять, что сохранённая через конструктор строка соответствует требованиям.
-             Если строка удовлетворяет условиям, метод возвращает true, иначе — false.
-         */
-        if (name.isEmpty()) {
-            System.out.println("Пустое имя");
-            return false;
-        } else if (name.length() < 3 || name.length() > 19) {
-            System.out.println("Имя должно содержать от 3 до 19 символов");
-            return false;
-        } else if (Character.isWhitespace(name.charAt(0)) || Character.isWhitespace(name.charAt(name.length() - 1))) {
-            System.out.println("Имя не должно начинаться или заканчиваться пробелом");
-            return false;
-        } else if (name.length() - name.replace(" ", "").length() > 1) {
-            System.out.println("Имя не содержит пробела или содержит более одного пробела");
-            return false;
-        } System.out.println("Карта выпущена: " + name);
-        return true;
+        boolean result = false;
+        if (name != null){
+            result = name.matches("(?=^.{3,19}$)(\\p{L}+\\s\\p{L}+)$");
+        }
+        return result;
     }
-
 }
